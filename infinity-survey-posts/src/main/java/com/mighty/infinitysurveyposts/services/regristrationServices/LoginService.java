0b06cf4b18ea76a45eb5f796ab73390bd1746679
Hatice <hatice.karabacak@ing.com>
2024-03-18 17:29:21 +0100

    Changes from Off-InfiniSurv-Project transmitted

--- infinity-survey-posts/src/main/java/com/mighty/infinitysurveyposts/services/regristrationServices/LoginService.java
+++ infinity-survey-posts/src/main/java/com/mighty/infinitysurveyposts/services/regristrationServices/LoginService.java
@@ -2,6 +2,8 @@ package com.mighty.infinitysurveyposts.services.regristrationServices;
 
 import com.mighty.infinitysurveyposts.models.UserModel;
 import com.mighty.infinitysurveyposts.repositorys.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,7 @@ public class LoginService {
     @Autowired
     UserRepository userRepository;
     private boolean status;
-
+    private static final Logger logger = LogManager.getLogger(LoginService.class);
     public ResponseEntity<Boolean> loginAuthentification (UserModel user){
 
         UserModel userEmail = userRepository.findByEmail(user.getEmail());
